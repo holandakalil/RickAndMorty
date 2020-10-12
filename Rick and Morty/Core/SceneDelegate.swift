6c68a154ebb2daf28c83b9094bcc9c50681e7c29
Kalil Holanda <kalilholanda@me.com>
2020-10-11 23:41:03 -0300

    Initial setup for using ViewCode and project organisation.

---
+++ Rick and Morty/Core/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  Rick and Morty
+//
+//  Created by Kalil Holanda on 11/10/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+    
+}
+
--- ViewController.swift
+++ ViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds) // Making sure everything is working
+        view.backgroundColor = .red
+        self.view = view
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
